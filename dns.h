a3c9fedbb3ec4438a722dca0d0380d7193ccc199
Minhyeok Jeon <minhyeogjeon945@gmail.com>
2025-01-08 17:52:24 +0900

    dns
    get_host_name() & get_host_entry(string)

--- dns.h
+++ dns.h
@@ -1,6 +1,16 @@
 #pragma once
 
+#include "native.h"
+
+struct host_entry
+{
+	string host_name;
+};
+
 class dns
 {
+public:
+	static string get_host_name();
+	static host_entry get_host_entry(string host);
 };
 

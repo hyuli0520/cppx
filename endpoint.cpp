d3cfccf2badde7b80e135f85d45d3f0de1391fd3
minhyeok <minhyeogjeon945@gmail.com>
2025-01-09 17:35:43 +0900

    get & set
    get address, port / set port

--- endpoint.cpp
+++ endpoint.cpp
@@ -7,3 +7,17 @@ endpoint::endpoint(ip_address address, u_short port)
 	_address.sin_port = htons(port);
 }
 
+ip_address endpoint::get_address() const
+{
+	return _address;
+}
+
+u_short endpoint::get_port()
+{
+	return ntohs(_address.sin_port);
+}
+
+void endpoint::set_port(u_short port)
+{
+	_address.sin_port = htons(port);
+}
--- endpoint.h
+++ endpoint.h
@@ -8,5 +8,12 @@ class endpoint
 public:
 	endpoint() = default;
 	endpoint(ip_address address, u_short port);
+
+	ip_address get_address() const;
+	u_short get_port();
+	void set_port(u_short port);
+
+private:
+	ip_address _address;
 };
 

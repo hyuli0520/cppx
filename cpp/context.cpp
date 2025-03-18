ee37a611fb975d0751b14b9015b5462a3f018932
hyuli0520 <minhyeogjeon945@gmail.com>
2025-03-18 14:18:14 +0900

    socket empty error solved

--- cpp/context.cpp
+++ cpp/context.cpp
@@ -13,8 +13,6 @@ context::~context()
 
 void context::init()
 {
-	_socket.reset();
-	_buffer.clear();
 	_io_type = io_type::none;
 	ZeroMemory(static_cast<OVERLAPPED*>(this), sizeof(OVERLAPPED));
 }
